998e89ff3dfbf284ecc0d44d68c62bea2331a6ef
Jonics7 <jonics7@mail.ru>
2022-03-10 20:44:00 +0300

    mobile & fixes

--- src/App.tsx
+++ src/App.tsx
@@ -45,17 +45,15 @@ function App() {
         <div className="App">
             <div className="App-content">
                 <img src={album} alt="album" className="App-image" draggable={false} />
-                <div className="App-content-right">
-                    <div className="App-vertical">Mirrors// Entourage</div>
-                    <div className="App-socials">
-                        {socialLinks.map((link) => (
-                            <SocialLink {...link} />
-                        ))}
-                    </div>
-                </div>
-            </div>
-            <div className="App-player-holder">
                 <Player />
+            </div>
+            <div className="App-right">
+                <div className="App-vertical">Mirrors// Entourage</div>
+                <div className="App-socials">
+                    {socialLinks.map((link, idx) => (
+                        <SocialLink {...link} key={idx} />
+                    ))}
+                </div>
             </div>
             <img src={logo} alt="logo" className="App-logo" />
         </div>
